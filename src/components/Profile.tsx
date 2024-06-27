0d07f59c28560682e6a3f2376cdc1038f1bfe734
Sergio <sergiocarrascal000@gmail.com>
2024-06-27 12:23:11 +0200

    profile text format

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -10,11 +10,13 @@ export const Profile = () => {
       >
         ¿Quienes somos?
       </div>
-      <div className="flex flex-col w-full lg:flex-row">
+      <div className="flex flex-col w-full lg:flex-row leading-10">
         <div className="flex-1 h-auto card  rounded-box place-items-center p-3 text-justify ml-3 mr-3">
           <img src={rocio} alt="Rocío Gentil"  style={{ width: '70%', height: '70%' }}/>
-          <p className="ml-1">
-            Mª del Rocío Gentil. Psicóloga especializada en intervención
+          <p className="ml-1 mt-4">
+
+          <span className="font-bold text-xl">Mª del Rocío Gentil. </span>
+            Psicóloga especializada en intervención
             criminológica y victimológica. Psico/sociodramatista. Colegiada en
             el Colegio Oficial de Psicólogos de Castilla y León (COPCYL).
             Miembro de la Sociedad Española de Psicoterapia y técnicas de grupo
@@ -26,10 +28,12 @@ export const Profile = () => {
         <div className="divider lg:divider-horizontal"></div>
         <div className="flex-1 h-auto card  rounded-box place-items-center p-3 ml-3 mr-3">
         <img src={sergio} alt="Sergio Carrascal"  style={{ width: '70%', height: '70%' }}/>
-          Sergio Carrascal. Músico graduado en el Conservatorio Profesional de
-          Música de Salamanca. Biólogo (USAL). Colaborador en varios grupos de
-          música de cámara. Clarinetista en la Banda de Música Tomás Bretón.
-          Músico de la compañía de Teatro Playback “Entrespejos” desde 2017
+        <p className="ml-1 mt-4">
+        <span className="font-bold text-xl">Sergio Carrascal. </span>
+          Músico graduado en el Conservatorio Profesional de
+          Música de Salamanca. Biólogo (USAL). Organizador y colaborador en varios ensembles y grupos de
+          música de cámara. Clarinetista en la banda de música Tomás Bretón y músico de la compañía de Teatro Playback “Entrespejos” desde 2017.
+          </p>
         </div>
       </div>
     </>
